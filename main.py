a35e9995dd8a0fdfd1763f572e8d5f00c61c2d9f
afons0silv42006-design <afons0.silv4.2006@gmail.com>
2025-12-31 17:14:23 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ echo "Data/Hora: $(date)"
 CPU_LOAD=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
 echo -e "Uso de CPU: ${VERDE}${CPU_LOAD}%${RESET}"
 
-# Uso de Memória RAM
+# Uso de Memoria RAM
 MEM_TOTAL=$(free -m | awk '/Mem:/ {print $2}')
 MEM_USADA=$(free -m | awk '/Mem:/ {print $3}')
 MEM_PERC=$(( MEM_USADA * 100 / MEM_TOTAL ))
 
-echo -e "Memória RAM: ${VERDE}${MEM_USADA}MB / ${MEM_TOTAL}MB (${MEM_PERC}%)${RESET}"
+echo -e "Memoria RAM: ${VERDE}${MEM_USADA}MB / ${MEM_TOTAL}MB (${MEM_PERC}%)${RESET}"
 
 # Uso de Disco
 DISCO_PERC=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
@@ -20,11 +20,11 @@ echo -e "Espaço em Disco (/): ${VERDE}${DISCO_PERC}% usado${RESET}"
 # Alertas
 echo "---------------------------------------"
 if [ "$MEM_PERC" -gt 80 ]; then
-    echo -e "${VERMELHO} ALERTA: Uso de Memória RAM acima de 80%${RESET}"
+    echo -e "${VERMELHO} ALERTA: Uso de Memoria RAM acima de 80%${RESET}"
 fi
 
 if [ "$DISCO_PERC" -gt 90 ]; then
-    echo -e "${VERMELHO} ALERTA: Pouco espaço em disco disponível${RESET}"
+    echo -e "${VERMELHO} ALERTA: Pouco espaço em disco disponivel${RESET}"
 fi
 
-echo "Verificação concluída."
+echo "Verificação concluida."
