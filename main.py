ef2149c595de2aa2082c8e3e70ab226526eb411e
afons0silv42006-design <afons0.silv4.2006@gmail.com>
2026-01-07 10:14:55 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,22 @@
 
 echo "Data/Hora: $(date)"
 
-# Uso de CPU
+#Uso de CPU
 CPU_LOAD=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
 echo -e "Uso de CPU: ${VERDE}${CPU_LOAD}%${RESET}"
 
-# Uso de Memoria RAM
+#Uso de Memoria RAM
 MEM_TOTAL=$(free -m | awk '/Mem:/ {print $2}')
 MEM_USADA=$(free -m | awk '/Mem:/ {print $3}')
 MEM_PERC=$(( MEM_USADA * 100 / MEM_TOTAL ))
 
 echo -e "Memoria RAM: ${VERDE}${MEM_USADA}MB / ${MEM_TOTAL}MB (${MEM_PERC}%)${RESET}"
 
-# Uso de Disco
+#Uso de Disco
 DISCO_PERC=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
 echo -e "Espaço em Disco (/): ${VERDE}${DISCO_PERC}% usado${RESET}"
 
-# Alertas
+#Alertas
 echo "---------------------------------------"
 if [ "$MEM_PERC" -gt 80 ]; then
     echo -e "${VERMELHO} ALERTA: Uso de Memoria RAM acima de 80%${RESET}"
